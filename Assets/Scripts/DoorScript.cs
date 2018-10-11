9eaba504c9d57f675cc44fd2410557a7bb47409d
Ivan Puljic <ivan.puljic.48@outlook.com>
2018-10-11 22:06:32 +0200

    muh doors

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+    
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponentInChildren<Animator>();
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("entered");
+        if (other.tag == "Player" && Input.GetButtonDown("Interact"))
+        {
+            animator.SetBool("close", false);
+            animator.SetBool("open", true);
+        }
+    }
+
+    /*private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("entered");
+        if (other.tag == "Player")
+        {
+            animator.SetBool("close", false);
+            animator.SetBool("open", true);
+        }
+    }*/
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("exited");
+        if (other.tag == "Player")
+        {
+            animator.SetBool("open", false);
+            animator.SetBool("close", true);
+        }
+    }
+}
---
+++ Assets/Scripts/ToggleCollider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleCollider : MonoBehaviour {
+
+    private MeshCollider colider;
+
+    // Use this for initialization
+    void Start () {
+        colider = GetComponent<MeshCollider>();
+    }
+
+    public void toggle()
+    {
+        if (colider.enabled == true)
+        {
+            colider.enabled = false;
+        }
+        else
+        {
+            colider.enabled = true;
+        }
+    }
+}
